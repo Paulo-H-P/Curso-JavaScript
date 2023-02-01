1a359907cb450559fec01129cc8af548efabbf2a
Paulo-H-P <fredysoh@gmail.com>
2023-02-01 20:45:32 -0300

    att curso js 01 02 2023

---
+++ jsCursoEmVideo/aula007.js
@@ -0,0 +1,19 @@
+/*Condições - if / else*/
+/*let vel = 50;
+console.log(`A velocidade do carro é de ${vel} km/h`);
+if (vel > 60) {
+  console.log(`Você ultrapassou a velocidade permitida, MULTADO`);
+} else {
+  console.log(`Dirija sempre com o cinto de segurança`);
+}
+
+/*------------------------------------------------------*/
+
+let pais = 'Brasil';
+
+console.log(`Vivendo em ${pais}`);
+if (pais != 'Brasil') {
+  console.log('Você é Estrangeiro');
+} else {
+  console.log('Você é Brasileiro');
+}
---
+++ jsCursoEmVideo/aula008.js
@@ -0,0 +1,27 @@
+/*teste if , else if e else*/
+
+/*let idade = 70;
+console.log(`Você tem ${idade}`);
+
+if (idade < 16) {
+  console.log('Você não tem a idade minima para votar');
+} else if (idade < 18 || idade > 65) {
+  console.log('Voto opcional');
+} else {
+  console.log('Voto obrigatorio');
+}
+
+/* horario */
+
+let agora = new Date();
+let horas = agora.getHours();
+
+console.log(`Agora são ${horas} Horas`);
+
+if (horas < 12) {
+  console.log('Bom dia');
+} else if (horas <= 18) {
+  console.log('Boa tarde');
+} else {
+  console.log('Boa noite');
+}
