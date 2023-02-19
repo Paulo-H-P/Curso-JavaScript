f2762d6744755b995cbca886202cd7eed3aa91d1
Paulo-H-P <fredysoh@gmail.com>
2023-02-19 12:46:17 -0300

    att exe 19 02 2023

--- jsCursoEmVideo/exer15/script.js
+++ jsCursoEmVideo/exer15/script.js
@@ -1,16 +1,22 @@
-function clicar() {
-  let num = document.getElementById('num');
-  let res = document.getElementById('res');
-  let calculo = '';
+function tabuada() {
+  let num = document.getElementById('txtn');
+  let tab = document.getElementById('seltab');
 
-  let num1 = Number(num.value);
-
-  if (num1 == 0 || num1 == '') {
+  if (num.value.length == 0) {
     window.alert('[ERRO] Dados invalidos');
   } else {
-    res.innerHTML = `Tabuada do Numero: ${num1}</br>`;
-
-    for (let c = 1; c <= 10; c++)
-      res.innerHTML += `${num1}x${c}=${num1 * c}<br>`;
+    let n = Number(num.value);
+    let c = 1;
+    tab.innerHTML = '';
+    while (c <= 10) {
+      let item =
+        document.createElement(
+          'option'
+        ); /* vai criar elemento de escolha dentro do select
+      é como se no select do html fosse criado as tags options com os valores*/
+      item.text = `${n} x ${c} = ${n * c}`; //item.txt são os valores que vão ficar dentro das opções criadas dentro do select
+      tab.appendChild(item);
+      c++;
+    }
   }
 }
