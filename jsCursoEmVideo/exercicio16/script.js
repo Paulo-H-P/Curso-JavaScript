301ec0e7957f06b902a33d46487e93c788315d87
Paulo-H-P <fredysoh@gmail.com>
2023-02-26 01:28:50 -0300

    exer att 26 02 2023

--- jsCursoEmVideo/exercicio16/script.js
+++ jsCursoEmVideo/exercicio16/script.js
@@ -1,5 +1,33 @@
-function adicionar() {
-  let numero = document.getElementById('numero');
-  let lista = document.getElementById('lista');
+let num = document.querySelector('input#fnum');
+let lista = document.querySelector('select#flista');
+let res = document.querySelector('div#res');
+let valores = []; //vetor
 
-  
+function isNumero(n) {
+  if (Number(n) >= 1 && Number(n) <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function inLista(n, l) {
+  if (l.indexOf(Number(n)) != -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function adicionar() {
+  if (isNumero(num.value) && !inLista(num.value, valores)) {
+    //! quer dizer não
+
+    valores.push(Number(num.value));
+    let item = document.createElement('option');
+    item.txt = `valor ${num.value} adicionado`;
+    lista.appendChild(item);
+  } else {
+    window.alert('valor invalido ou já encontrado na lista');
+  }
+}
