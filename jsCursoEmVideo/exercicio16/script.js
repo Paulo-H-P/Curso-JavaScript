5f12f5e3b41878d6d352f3be3eefbda011ea9757
Paulo-H-P <fredysoh@gmail.com>
2023-03-03 20:29:41 -0300

    att exercicios 03 03 2023

--- jsCursoEmVideo/exercicio16/script.js
+++ jsCursoEmVideo/exercicio16/script.js
@@ -25,9 +25,43 @@ function adicionar() {
 
     valores.push(Number(num.value));
     let item = document.createElement('option');
-    item.txt = `valor ${num.value} adicionado`;
+    item.text = `Valor ${num.value} adicionado`;
     lista.appendChild(item);
+    res.innerHTML = '';
   } else {
     window.alert('valor invalido ou já encontrado na lista');
+  }
+  num.value = ''; //limpa a lista
+  num.focus(); // curso volta a piscar
+}
+
+function finalizar() {
+  if (valores.length == 0) {
+    window.alert('Adicione valores antes de finalizar');
+  } else {
+    let tot = valores.length;
+
+    let maior = valores[0];
+    let menor = valores[0];
+    let soma = 0
+    let media = 0
+
+    for
+    for (let pos in valores) {
+      soma += valores[pos]
+      if (valores[pos] > maior) {
+        maior = valores[pos];
+        if (valores[pos] < menor) {
+          menor = valores[pos];
+        }
+      }
+    }
+    media = soma / tot
+    res.innerHTML = '';
+    res.innerHTML += `<p> Ao todo, temos ${tot} numeros cadastrados </p>`;
+    res.innerHTML += `<p> o maior valor informado foi ${maior} </p>`;
+    res.innerHTML += `<p> o menor valor informado foi ${menor} </p>`;
+    res.innerHTML += `<p> somando todos os valores , temos ${soma}</p>`
+    res.innerHTML += `<p> a media dos valores digitados é ${media}</p>`
   }
 }
---
+++ jsCursoEmVideo/teste estagio/teste.js
@@ -0,0 +1,10 @@
+let n = 13;
+let soma = 0;
+let n1 = 0;
+
+while (n1 < n) {
+  n1 = n1 + 1;
+  soma = soma + n1;
+}
+
+console.log(soma);
